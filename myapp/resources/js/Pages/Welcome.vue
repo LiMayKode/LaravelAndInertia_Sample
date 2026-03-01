ceb3aa6b6cb11be2f78746c8f5a1669c395e4db9
LiMayCode <lloyd.mark.parnecio@student.uno-r.edu.ph>
2026-03-01 11:55:37 +0800

    Initial commit: Laravel + Vue + Inertia with minimalist ロイド landing page

---
+++ myapp/resources/js/Pages/Welcome.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="ロイド" />
+    <div class="min-h-screen bg-black flex items-center justify-center">
+        <h1 class="text-8xl md:text-9xl font-light text-white tracking-widest">
+            ロイド
+        </h1>
+    </div>
+</template>
